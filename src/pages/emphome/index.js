1749c19d5013794f8acf53b093d20224b622aebe
cosmicparticle <wnq111@126.com>
2020-04-14 17:52:22 +0800

    echart实验

---
+++ src/pages/emphome/index.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import './index.css'
+import { Row, Col } from 'antd';
+import AnyEchart from "./AnyEchart";
+import LineEchart from "./LineEchart";
+import AniEchart from "./AniEchart";
+import ThreeTypeEchart from "./ThreeTypeEchart";
+import LiquidFillEchart from "./LiquidFillEchart";
+import RootTypeEchart from "./RootTypeEchart";
+import ColTypeEchart from "./ColTypeEchart";
+import ColTypeEchart2 from "./ColTypeEchart2";
+/**
+const HOME_CONFIG = {
+    component       : HomePage,
+    blockIds        : null,
+    excludeBlockIds : null
+};
+**/
+export default class Home extends React.Component{
+
+
+        render(){
+            const titleStyle={
+                color:'black',
+                fontSize:'30px',
+                paddingBottom:'20px',
+                //border:'1px solid blue',
+            };
+            return (
+                <div >
+                    <Row  >
+                        <Col className="gutter-row" style={titleStyle} align={"middle"}>
+                            代办工作
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <ThreeTypeEchart />
+                        </Col>
+                        <Col span={12}>
+                            <ThreeTypeEchart />
+                        </Col>
+                    </Row>
+                    <Row  >
+                        <Col className="gutter-row" style={titleStyle} align={"middle"}>
+                            已就业情况分析
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={6}>
+                            <LiquidFillEchart />
+                        </Col>
+                        <Col span={9}>
+                            <AnyEchart />
+                        </Col>
+                        <Col span={9}>
+                            <ColTypeEchart />
+                        </Col>
+                    </Row>
+                    <Row  >
+                        <Col className="gutter-row" style={titleStyle} align={"middle"}>
+                            待就业情况分析
+                        </Col>
+                    </Row>
+                    <Row type={"flex"} align={"middle"} justify={"space-around"}>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                        <Col span={3}>
+                        <RootTypeEchart />
+                    </Col>
+                        <Col span={3}>
+                            <RootTypeEchart />
+                        </Col>
+                    </Row>
+
+                </div>
+        );
+    }
+}
